2ad828c93dcebe64e973de7097aaa00086432c1c
a2175 <as217512@gmail.com>
2024-12-04 19:36:21 +0900

    init

---
+++ src/main/java/hello/springmsaeurekaserver/SpringmsaeurekaserverApplication.java
@@ -0,0 +1,13 @@
+package hello.springmsaeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmsaeurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmsaeurekaserverApplication.class, args);
+	}
+
+}
