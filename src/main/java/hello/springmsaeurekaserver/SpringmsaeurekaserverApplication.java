98bda668f58935da66edc1ed0b3980936ceed85e
a2175 <as217512@gmail.com>
2024-12-04 19:41:19 +0900

    스프링 클라우드 MSA 6 : Eureka 서버 구축
    https://www.youtube.com/watch?v=OXDwOYxKvWA&list=PLJkjrxxiBSFBPk-6huuqcjiOal1KdU88R&index=7

--- src/main/java/hello/springmsaeurekaserver/SpringmsaeurekaserverApplication.java
+++ src/main/java/hello/springmsaeurekaserver/SpringmsaeurekaserverApplication.java
@@ -2,8 +2,10 @@ package hello.springmsaeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringmsaeurekaserverApplication {
 
 	public static void main(String[] args) {
